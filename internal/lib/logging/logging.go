f962b463f284fdf4641a98f692484f0681daff57
Andreas <git@fsck.rip>
2025-12-26 21:05:25 -0500

    chore: add logging
    Signed-off-by: Andreas <git@fsck.rip>

---
+++ internal/lib/logging/logging.go
@@ -0,0 +1,18 @@
+package logging
+
+import (
+	"log/slog"
+	"os"
+)
+
+func BuildLogger(method string) *slog.Logger {
+	level := slog.LevelInfo
+	if os.Getenv("ENV") != "prod" {
+		level = slog.LevelDebug
+	} else {
+		level = slog.LevelInfo
+	}
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level})).With("method", method)
+	return logger
+}
